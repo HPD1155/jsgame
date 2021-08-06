c71f85ce0fb26cafd3a8dfa809168fe2276c608a
Hack.cpp <72662612+HPD1155@users.noreply.github.com>
2021-08-06 22:42:57 +0000

    i made a square

--- script.js
+++ script.js
@@ -2,5 +2,9 @@ function loaded()
 {
   // start
   console.log("THIS IS THE FIRST QUANTUM GAME STUDIO GAME! A WEB BASED GAME U CAN PLAY! - QUANTUMXS")
-  
+  // canvas stuff
+  var canvas = document.getElementById("cvs")
+  var context = canvas.getContext("2d")
+  // render
+  context.fillRect(200, 10, 100, 100)
 }
