70c5c2e6f4d233ae2a1622ef1bc3f630625b6069
Hack.cpp <72662612+HPD1155@users.noreply.github.com>
2021-08-06 22:59:28 +0000

    TRIANGLE!!!!!111

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ function loaded()
   console.log("THIS IS THE FIRST QUANTUM GAME STUDIO GAME! A WEB BASED GAME U CAN PLAY! - QUANTUMXS")
   // canvas stuff
   var canvas = document.getElementById("cvs")
-  var context = canvas.getContext("2d")
+  var cntxt = canvas.getContext("2d")
   // render
-  context.fillRect(200, 10, 100, 100)
+  cntxt.beginPath();
+  cntxt.moveTo(10, 120);
+  cntxt.lineTo(10, 180);
+  cntxt.lineTo(110, 150);
+  cntxt.fill();
 }
